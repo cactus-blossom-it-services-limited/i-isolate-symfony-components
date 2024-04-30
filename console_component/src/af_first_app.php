a7f4be272fb2414b0404947d47a15bfa73f15c9d
Andrew Farquharson <farquharson.andrew@royalmail.com>
2024-05-01 00:32:40 +0100

    add doc comments

--- console_component/src/af_first_app.php
+++ console_component/src/af_first_app.php
@@ -1,6 +1,10 @@
 #!/usr/bin/env php
 <?php
-// https://symfony.com/doc/6.4/components/console.html
+// See: https://symfony.com/doc/6.4/components/console.html
+// See: https://dev.to/pretzelhands/building-an-easily-distributable-single-file-command-line-app-with-php-and-composer-2hfi
+// Run this command:
+// #./af_first_app.php --bar=elephant-tusk
+
 require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,9 +20,10 @@ use Symfony\Component\Console\SingleCommandApplication;
     ->addOption('bar', null, InputOption::VALUE_REQUIRED)
     ->setCode(function (InputInterface $input, OutputInterface $output): int {
         // output arguments and options
+        $bar = $input->getOption('bar');
+        $output->writeln($bar);
         $output->writeln("Some text on screen!");
         $output->writeln("Some more output");
         return 4;
-
     })
     ->run();
